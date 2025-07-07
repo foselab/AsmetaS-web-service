a94afef8bb83fab4017183a732b00ce15926bde1
scandurra <patrizia.scandurra@unibg.it>
2025-07-07 12:17:05 +0200

    Update main.py

--- examples/medicine_reminder_example/main.py
+++ examples/medicine_reminder_example/main.py
@@ -7,7 +7,7 @@ import os
 import sys
 import uuid
 import time
-# Aggiunge la directory padre al sys.path
+# Add the parent directory to the sys.path
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import logging_manager
